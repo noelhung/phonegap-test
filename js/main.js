c9ac20edf6e5415dedd8e15513b7dd02212b9d8a
Noel Hung <noel@memoriki.com>
2013-10-10 21:29:08 +0800

    Part 8: Highlighting Tapped or Clicked UI Elements
    touchstart & touchend VS mousedown & mouseup

--- js/main.js
+++ js/main.js
@@ -24,7 +24,29 @@ var app = {
 			alert(title ? (title + ": " + message) : message);
 		}
 	},
-
+	
+	registerEvents: function() {
+	    var self = this;
+	    // Check of browser supports touch events...
+	    if (document.documentElement.hasOwnProperty('ontouchstart')) {
+	        // ... if yes: register touch event listener to change the "selected" state of the item
+	        $('body').on('touchstart', 'a', function(event) {
+	            $(event.target).addClass('tappable-active');
+	        });
+	        $('body').on('touchend', 'a', function(event) {
+	            $(event.target).removeClass('tappable-active');
+	        });
+	    } else {
+	        // ... if not: register mouse events instead
+	        $('body').on('mousedown', 'a', function(event) {
+	            $(event.target).addClass('tappable-active');
+	        });
+	        $('body').on('mouseup', 'a', function(event) {
+	            $(event.target).removeClass('tappable-active');
+	        });
+	    }
+	},
+	
 	initialize: function() {
 	    var self = this;
 	    this.store = new MemoryStore(function() {
@@ -71,3 +93,4 @@ var app = {
 };
 
 app.initialize();
+app.registerEvents();
