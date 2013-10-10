491fd80fe26307169dd82c3ecec3ba6348601cdf
Noel Hung <noel@memoriki.com>
2013-10-10 20:58:40 +0800

    Part 7: Adding Styles and Touch-Based Scrolling
    hide scrollbar but able to scroll

--- js/HomeView.js
+++ js/HomeView.js
@@ -15,8 +15,16 @@ var HomeView = function(store) {
 	};
 	
 	this.findByName = function() {
-	    store.findByName($('.search-key').val(), function(employees) {
+	    store.findByName($('.search-key').val(), function(employees) { 
 	        $('.employee-list').html(HomeView.liTemplate(employees));
+	        if (self.iscroll) {
+	            console.log('Refresh iScroll');
+	            self.iscroll.refresh(); //make the scroll make on current length
+	        } else {
+	            console.log('New iScroll');
+	            //initialise if not exist
+	            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+	        }
 	    });
 	};
 
