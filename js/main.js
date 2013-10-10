1b1f67b335a9f608ed45311ae666f32dad5d2694
Noel Hung <noel@memoriki.com>
2013-10-10 19:44:39 +0800

    Part 6: Creating a View Class
    structure the view as class

--- js/main.js
+++ js/main.js
@@ -15,13 +15,6 @@ var app = {
 		});
 	},
 
-
-	// Part 5: use handlebar template
-	renderHomeView: function() {
-	    $('body').html(this.homeTpl());
-	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-	
 	
 	// Part 3: Native Notification
 	showAlert : function(message, title) {
@@ -32,47 +25,40 @@ var app = {
 		}
 	},
 
-	// Part 5: use HandleBar Templates
-	findByName: function() {
+	initialize: function() {
 	    var self = this;
-	    this.store.findByName($('.search-key').val(), function(employees) {
-	        $('.employee-list').html(self.employeeLiTpl(employees));
+	    this.store = new MemoryStore(function() {
+	        $('body').html(new HomeView(self.store).render().el);
 	    });
-	},
-	
-	initialize : function() {
-		var self = this;
-		
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
 
-        this.store = new MemoryStore(function() {
-            self.renderHomeView();
-        });
-        
-        
-		// Part 3: Native Notification
-		if (false) {
-			// Part 3: Native Notification
-			this.store = new MemoryStore(function() {
-				// will run it after it has successfully initialized
-					self.showAlert('Store Initialized', 'Info');
-				});
-			$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	
-		}
-		// Part 4: Use single page application
-		if (false) {
-	
-			this.store = new MemoryStore(function() {
-				// use js to implement home view
-					self.renderHomeView();
-				});
-		}
+	    
 		
 	}
-
 	/*
+	this.homeTpl = Handlebars.compile($("#home-tpl").html());
+	this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+
+    this.store = new MemoryStore(function() {
+        self.renderHomeView();
+    });
+    
+    
+	// Part 3: Native Notification
+		// Part 3: Native Notification
+		this.store = new MemoryStore(function() {
+			// will run it after it has successfully initialized
+				self.showAlert('Store Initialized', 'Info');
+			});
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+
+	
+	// Part 4: Use single page application
+
+		this.store = new MemoryStore(function() {
+			// use js to implement home view
+				self.renderHomeView();
+			});
+
 	// Part 4: Use single page application
 	renderHomeView : function() {
 		var html = "<div class='header'><h1>Home</h1></div>"
