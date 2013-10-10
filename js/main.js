ed047c01e4a07131a9ca0b556fb19645f986ee60
Noel Hung <noel@memoriki.com>
2013-10-10 15:59:02 +0800

    Part 5: Use JS to add HTML

--- js/main.js
+++ js/main.js
@@ -13,23 +13,47 @@ var app = {
         });
     },
     
+    //Part 5: Use js to render HTML
+    renderHomeView: function() {
+        var html =
+                "<div class='header'><h1>Home</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+    
+    //Part 4: Native Notification
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
             alert(title ? (title + ": " + message) : message);
-        }
+        } 
     },
-    
+     
     
     initialize: function() {
         var self = this;
-        this.store = new MemoryStore(function() {
-        	//will run it after it has successfully initialized
-            self.showAlert('Store Initialized', 'Info');
-        });
+        if (false){
+        	//Part 4: Native Notification
+	        this.store = new MemoryStore(function() {
+	        	//will run it after it has successfully initialized
+	            self.showAlert('Store Initialized', 'Info');
+	        });
+	        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	        
+        }else{
+        	
+        	//Part 5: Use js to render HTML
+	        this.store = new MemoryStore(function() {
+	        	//use js to implement home view
+	            self.renderHomeView();
+	        });
+        }
         
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
         
     }
 
